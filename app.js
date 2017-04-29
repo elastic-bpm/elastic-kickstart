937e18ee2b67937c4eb2db9e8cd052c2c6186f05
Johannes Bertens <johannes.bertens@gmail.com>
2017-04-29 13:26:49 +0200

    Goodbye docker

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var msRestAzure = require('ms-rest-azure');
 var ComputeClient = require('azure-arm-compute');
 
-var clientId = process.env.CLIENT_ID;
-var domain = process.env.DOMAIN;
-var secret = process.env.APPLICATION_SECRET;
-var subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
+if (process.argv.length < 3) {
+    console.log("Usage: node app.js <location-to-config> [start/stop]");
+    return;
+}
 
-var action = process.argv[2] || process.env.ACTION || "stop";
+var config = require(process.argv[2]);
+var clientId = config.CLIENT_ID;
+var domain = config.DOMAIN;
+var secret = config.APPLICATION_SECRET;
+var subscriptionId = config.AZURE_SUBSCRIPTION_ID;
+
+// var clientId = process.env.CLIENT_ID;
+// var domain = process.env.DOMAIN;
+// var secret = process.env.APPLICATION_SECRET;
+// var subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
+
+var action = process.argv[3] || process.env.ACTION || "stop";
 
 
 var credentials = new msRestAzure.ApplicationTokenCredentials(clientId, domain, secret);
