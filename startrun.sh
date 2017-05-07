1801c3f3409b8007c103643f9c7ba6667c5b4a76
Johannes <johannes.bertens@gmail.com>
2017-05-07 17:52:18 +0200

    4 differen run scripts now

--- startrun.sh
+++ startrun.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Wait for swarm1 to be up
-command="ssh -q swarm1-master exit"
+command="ssh -q $1 exit"
 eval $command
 ret=$?
 while [ $ret -gt "0" ]
@@ -14,9 +14,9 @@ done
 echo Executing script
 
 # Start up elastic
-ssh swarm1-master 'bash -s' < ./start_elastic.sh
+ssh $1 'bash -s' < ./start_elastic.sh
 
 # Wait for nodes to be ready
-ssh swarm1-master 'bash -s' < ./wait_for_nodes.sh
+ssh $1 'bash -s' < ./wait_for_nodes.sh
 
 
