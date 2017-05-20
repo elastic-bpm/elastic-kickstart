1256246799253d3f3f62b69b6139b07c531dfb83
Johannes <johannes.bertens@gmail.com>
2017-05-20 20:07:27 +0200

    updated test check and removing workers

--- start_test.sh
+++ start_test.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 echo Waiting for API to be ready
-until $(curl --output /dev/null --silent --head --fail http://elastic-swarm1.westeurope.cloudapp.azure.com:8080/api/running); do
+until $(curl --output /dev/null --silent --head --fail localhost:8080/api/running); do
     printf '.'
     sleep 5
 done
 
 echo Sending run command
-curl -H "Content-Type: application/json" -X POST -d '{"policy":"Static"}' http://elastic-swarm1.westeurope.cloudapp.azure.com:8080/api/runTest
+curl -H "Content-Type: application/json" -X POST -d '{"policy":"Static"}' localhost:8080/api/runTest
 
 echo Waiting for test to finish
-until $(curl --output /dev/null --silent --head --fail http://elastic-swarm1.westeurope.cloudapp.azure.com:8080/api/testDone); do
+until $(curl --output /dev/null --silent --head --fail localhost:8080/api/testDone); do
     printf '.'
     sleep 5
 done
 
-echo Test is done
+echo Test is done, deleting workers
+
+curl -X DELETE localhost:8080/api/docker/services/workers
+
+echo Done
 
